570edc15043066a7dbbba152a156cdf564271619
sancho6040 <56619610+sancho6040@users.noreply.github.com>
2021-11-16 02:26:16 -0500

    reapracion otra vez :v:

--- js/ligths.js
+++ js/ligths.js
@@ -22,20 +22,4 @@ function Ligths() {
         pointLightHelper2
     );
 
-    const lightP = new THREE.PointLight(0x11E5E5, 1, 200);
-    lightP.position.set(20, 0, 0);
-    scene.add(lightP);
-    lightP.castShadow = true
-    const sphereSize = 1;
-    const pointLightHelper = new THREE.PointLightHelper(lightP, sphereSize);
-    scene.add(pointLightHelper);
-
-    const light1 = new THREE.PointLight(0xc502b5, 1, 200);
-    light1.position.set(-20, 0, 0);
-    scene.add(light1);
-    light1.castShadow = true
-    const sphereSize1 = 1;
-    const pointLightHelper1 = new THREE.PointLightHelper(light1, sphereSize1);
-    scene.add(pointLightHelper1);
-
 }
--- js/player.js
+++ js/player.js
@@ -1,38 +1,17 @@
-function Player(path){
+function Player(miCamera){
 
-    const loader = new THREE.FBXLoader();
-    loader.load('js/3D/' + path, function (object) {
+    this.cam = miCamera;
+    const rayCast = new THREE.Raycaster();
+    const mouse = new THREE.Vetor2();
 
-        this.mixer = new THREE.AnimationMixer(object);
+    this.actualiar = function(){
+        rayCast.setFromCamera(mouse, this.cam);
 
-        const action = mixer.clipAction(object.animations[0]);
-        action.play();
+        const intersects = rayCast.intersectsObjects(scene.childres, true);
 
-        object.traverse(function (child) {
+        if(intersects.length > 0){
+            console.log("ssss");
 
-            if (child.isMesh) {
-
-                child.castShadow = true;
-                child.receiveShadow = true;
-                console.log(child.name);
-
-                if (child.name == "Beta_Surface") {
-                    child.material = new THREE.MeshLambertMaterial({
-                        color: 0xBCC0C0
-                    });
-                }
-
-                object.scale.set(0.2, 0.2, 0.2);
-                object.position.y = -20;
-
-                child.material.skinning = true;
-
-            }
-
-        });
-
-        object.position.x = posX;
-        scene.add(object);
-    });
-
+        }
+    }
 }
